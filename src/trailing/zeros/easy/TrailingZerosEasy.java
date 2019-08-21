ccb28b9a9bb309f00707668df0cb4c8c30e27e16
Anand Jha <anandjha.755@gmail.com>
2019-08-21 22:43:47 +0530

    Trailing zero solution added

--- src/trailing/zeros/easy/TrailingZerosEasy.java
+++ src/trailing/zeros/easy/TrailingZerosEasy.java
@@ -13,30 +13,21 @@ public class TrailingZerosEasy {
         Scanner scanner = new Scanner(System.in);
         int testCaseCount = scanner.nextInt();
 
-        int[] numArr = new int[testCaseCount];
+        long[] numArr = new long[testCaseCount];
         for (int i = 0; i < testCaseCount; i++) {
-            numArr[i] = scanner.nextInt();
+            numArr[i] = scanner.nextLong();
         }
         Arrays.stream(numArr).forEach(TrailingZerosEasy::printTrailZero);
     }
 
-    static void printTrailZero(int num) {
-        long total;
+    static void printTrailZero(long num) {
+        long div = 5;
 
-        int startZero = num / 5;
-
-
-        int div = 100;
-        int k = 1;
-        long extraZero = 0;
+        long totalZeroCount = 0;
         while (num / div != 0) {
-
-            extraZero = extraZero + ((num / div) * (k));
-
-            div = div * 10;
-            k++;
+            totalZeroCount = totalZeroCount + num / div;
+            div = div * 5;
         }
-        total = startZero + extraZero;
-        System.out.println(total);
+        System.out.println(totalZeroCount);
     }
 }
