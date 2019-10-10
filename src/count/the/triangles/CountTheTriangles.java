c55618d7cf1d07bb7d5aae9fbeed66a550937f9e
Anand Jha <anandjha.755@gmail.com>
2019-10-10 15:01:41 +0530

    New solution added and few existing code modified

--- src/count/the/triangles/CountTheTriangles.java
+++ src/count/the/triangles/CountTheTriangles.java
@@ -1,40 +1,47 @@
 package count.the.triangles;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class CountTheTriangles {
-    //TODO THIS IS BRUTE FORCE SOLUTION... GETTING TIME OUT
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int testCaseCount = Integer.parseInt(reader.readLine());
         while (testCaseCount-- > 0) {
             int noOfRods = Integer.parseInt(reader.readLine());
             int[] rodsArr = Arrays.stream(reader.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+
             int noOfTriangles = getNoOfTriangles(rodsArr, noOfRods);
-            System.out.println(noOfTriangles);
+            writer.write(noOfTriangles + "\n");
+            //writer.flush();
         }
+        writer.flush();
     }
 
     private static int getNoOfTriangles(int[] rodsArr, int noOfRods) {
 
+        Arrays.sort(rodsArr);
+
         int triangleCount = 0;
-        for (int i = 0; i < noOfRods; i++) {
-            for (int j = i + 1; j < noOfRods; j++) {
-                for (int k = j + 1; k < noOfRods; k++) {
-                    if (canTriangleBeFormed(rodsArr[i], rodsArr[j], rodsArr[k])) {
-                        triangleCount++;
-                    }
-                }
+        int i = 0;
+        int j = 1;
+        while (i < noOfRods - 2) {
+            int k = j + 1;
+            while ((k < noOfRods) && (rodsArr[i] + rodsArr[j] > rodsArr[k])) {
+                k++;
+            }
+            triangleCount += (k - j - 1);
+            j++;
+            if (j == noOfRods) {
+                i++;
+                j = i + 1;
             }
         }
 
         return triangleCount;
-    }
-
-    private static boolean canTriangleBeFormed(int a, int b, int c) {
-        return ((a + b > c) && ((b + c) > a) && ((a + c) > b));
     }
 }
--- src/count/the/triangles/CountTheTrianglesAlter.java
+++ src/count/the/triangles/CountTheTrianglesAlter.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class CountTheTrianglesAlter {
 
+    //This is My solution which was giving wrong solution
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
