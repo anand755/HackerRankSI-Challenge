d5f14b2690bec70fdf55b1547ff0d2c041838613
Anand Jha <anandjha.755@gmail.com>
2019-11-02 02:45:14 +0530

    MaximumAlternateNodesSum tried.

--- src/maximum/alternate/nodes/sum/MaximumAlternateNodesSum.java
+++ src/maximum/alternate/nodes/sum/MaximumAlternateNodesSum.java
@@ -20,11 +20,68 @@ public class MaximumAlternateNodesSum {
     }
 
     private static int getMaxAlterNodeSum(int[] arrInput, int arrLength) {
+        //We are doing Bottom Up + Post Order Traversals
+        int maxAlterNateSum = 0;
         Node root = prepareBst(arrInput, arrLength);
 
+        ValPacket rootValPacket = getMaxVal(root);
 
-        return 0;
+        maxAlterNateSum = Math.max(rootValPacket.includeVal, rootValPacket.excludeVal);
+
+        return Math.max(maxAlterNateSum, 0);
     }
+
+
+    private static ValPacket getMaxVal(Node root) {
+
+        //Assumption: Any given node should return 2 things. 1-> value including that node along with child node
+        //2 -> value excluding that node but with child node
+
+        //Main Logic: For a given node
+        // includeVal = Max of all the combination for left and right child nodes exclude val + current data
+        //excludeVal = Max of all the combination for left and right child nodes include val and
+        //leftInclude+rightExclude, rightInclude+leftExclude
+
+
+        //Base Condition: When node  is null then include value and exclude value will be 0
+
+        if (root == null) {
+            return new ValPacket(0, 0);
+        }
+
+        ValPacket leftValPacket = getMaxVal(root.left);
+        ValPacket rightValPacket = getMaxVal(root.right);
+
+        //taking all the combination and filtering out the max value
+
+        int includeVal_1 = leftValPacket.excludeVal + rightValPacket.excludeVal;
+        int includeVal_2 = leftValPacket.excludeVal + root.data;
+        int includeVal_3 = rightValPacket.excludeVal + root.data;
+        int includeVal_4 = leftValPacket.excludeVal + rightValPacket.excludeVal + root.data;
+
+        int includeVal = Math.max(Math.max(includeVal_1, includeVal_2), Math.max(includeVal_3, includeVal_4));
+
+
+        int excludeVal_1 = leftValPacket.includeVal;
+        int excludeVal_2 = rightValPacket.includeVal;
+        int excludeVal_3 = leftValPacket.includeVal + rightValPacket.includeVal;
+
+        int temp_excludeVal_123 = Math.max(Math.max(excludeVal_1, excludeVal_2), excludeVal_3);
+
+
+        int excludeVal_4 = leftValPacket.includeVal + rightValPacket.excludeVal;
+        int excludeVal_5 = leftValPacket.excludeVal + rightValPacket.includeVal;
+
+        int temp_excludeVal_45 = Math.max(excludeVal_4, excludeVal_5);
+
+        int excludeVal = Math.max(temp_excludeVal_123, temp_excludeVal_45);
+
+
+        return new ValPacket(includeVal, excludeVal);
+
+
+    }
+
 
     private static Node prepareBst(int[] arrInput, int arrLength) {
         Node root = new Node(arrInput[0]);
@@ -58,6 +115,16 @@ public class MaximumAlternateNodesSum {
             this.data = data;
             this.left = null;
             this.right = null;
+        }
+    }
+
+    private static class ValPacket {
+        int includeVal;
+        int excludeVal;
+
+        private ValPacket(int includeVal, int excludeVal) {
+            this.includeVal = includeVal;
+            this.excludeVal = excludeVal;
         }
     }
 }
