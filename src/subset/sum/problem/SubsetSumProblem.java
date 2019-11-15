81a66031f64c1026e8041982edcc34c7f70f96fb
Anand Jha <anandjha.755@gmail.com>
2019-11-16 02:17:59 +0530

    SubSetSum solved using Dynamic Problem.

--- src/subset/sum/problem/SubsetSumProblem.java
+++ src/subset/sum/problem/SubsetSumProblem.java
@@ -25,44 +25,53 @@ public class SubsetSumProblem {
         writer.flush();
     }
 
-    private static String isSubSetExist(int[] arrInput, int arrLength, int requiredSum) {
+    private static String isSubSetExist(int[] arrInput, int N, int S) {
 
-        Set<Integer> allPossibleSumSet = new HashSet<>();
+        boolean[][] dpTable = new boolean[N][S + 1];
+        Arrays.sort(arrInput);
 
-        //dp(i) => # set of all the sum that can be formed using first i elements
-        //dp(i) = arr[i] , ((dp(i-1)+arr[i]) )
-
-        IndexSumSet[] dpSet = new IndexSumSet[arrLength];
-        dpSet[0] = new IndexSumSet(new HashSet<>(arrInput[0]));
-
-        allPossibleSumSet.add(arrInput[0]);
+        //DP Expression : dp(i,j) => Is there is a subset of sum =j using first i elements from array
 
 
-        for (int i = 1; i < arrLength; i++) {
+        //BaseCondition 1: There is always sum =0 for all the N elements of the array
+        // (Ex: If you don't consider any number then sum =0, Hence setting as true)
+        for (int i = 0; i <= (N - 1); i++) {
+            dpTable[i][0] = true;
+        }
 
-            HashSet<Integer> currSumSet = new HashSet<>();
-            currSumSet.add(arrInput[i]);
-            allPossibleSumSet.add(arrInput[i]);
+        //Base Condition 2: You can not create any arbitrary sum by using only arr[0]
+        //Hence setting all the sum as false except arr[0] (arr[0] can create a sum of arr[0])
 
-            for (int num : dpSet[i - 1].sumSet) {
-                currSumSet.add(num + arrInput[i]);
-                allPossibleSumSet.add(num + arrInput[i]);
+        for (int j = 1; j <= S; j++) {
+
+            if (j == arrInput[0]) {
+                dpTable[0][j] = true;
+            } else {
+                dpTable[0][j] = false;
             }
+            //dpTable[0][j] = false;
+        }
+        //dpTable[0][arrInput[0]] = true;
 
-            dpSet[i] = new IndexSumSet(currSumSet);
+        //DP State
+        //dp(i,j) = dp(i-1,j) || dp(i-1,j-arr[i])
+        for (int i = 1; i <= (N - 1); i++) {
+            for (int j = 1; j <= S; j++) {
+
+                if (j < arrInput[i]) {
+
+                    dpTable[i][j] = dpTable[i - 1][j];
+
+                } else {
+
+                    dpTable[i][j] = dpTable[i - 1][j] || dpTable[i - 1][j - arrInput[i]];
+
+                }
+            }
         }
 
-        String isPresent = allPossibleSumSet.contains(requiredSum) ? "YES" : "NO";
-
+        //Answer : Is there exist a sum =S using all the 0 to N-1 elements from the array.
+        String isPresent = dpTable[N - 1][S] ? "YES" : "NO";
         return isPresent;
-    }
-
-    private static class IndexSumSet {
-
-        Set<Integer> sumSet;
-
-        private IndexSumSet(Set<Integer> sumSet) {
-            this.sumSet = sumSet;
-        }
     }
 }
